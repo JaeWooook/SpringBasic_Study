1e3a1512696fc3648ba5afebe6a14d3b92d2d183
JaeWook Jeong <br12345678@ajou.ac.kr>
2021-10-12 02:19:15 +0900

    add user list checking

--- springtest/src/main/java/springstudy/springtest/controller/MemberController.java
+++ springtest/src/main/java/springstudy/springtest/controller/MemberController.java
@@ -2,10 +2,13 @@ package springstudy.springtest.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import springstudy.springtest.domain.Member;
 import springstudy.springtest.service.MemberService;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -26,9 +29,18 @@ public class MemberController {
     public String create(MemberForm form) {//memberform에 name에 spring이라는 입력된값이 들어오게된다.
         Member member = new Member();
         member.setName(form.getName());
+        //여기에 null이 들어간다?
+        System.out.println(member.getName());
         //이렇게하면 member가 만들어진다.
 
         memberService.join(member);
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members",members);
+        return "members/memberList";
     }
 }
