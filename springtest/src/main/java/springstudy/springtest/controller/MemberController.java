840e702ea869cfcc050263048abfecc735650db6
JaeWook Jeong <br12345678@ajou.ac.kr>
2021-10-11 23:49:46 +0900

    add home, add user func

--- springtest/src/main/java/springstudy/springtest/controller/MemberController.java
+++ springtest/src/main/java/springstudy/springtest/controller/MemberController.java
@@ -2,6 +2,9 @@ package springstudy.springtest.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import springstudy.springtest.domain.Member;
 import springstudy.springtest.service.MemberService;
 
 @Controller
@@ -13,4 +16,19 @@ public class MemberController {
         this.memberService = memberService;
     }
 
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";//멤버스의 createForm이라는 곳으로 이동한다.
+        //template에서 찾아서 알아서 렌더링해준다.
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {//memberform에 name에 spring이라는 입력된값이 들어오게된다.
+        Member member = new Member();
+        member.setName(form.getName());
+        //이렇게하면 member가 만들어진다.
+
+        memberService.join(member);
+        return "redirect:/";
+    }
 }
